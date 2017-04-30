86980db98469504ddf49250951457618a951b9ae
morgan greenwalt <you@example.com>
2017-04-29 20:54:10 -0500

    changes

--- game.js
+++ game.js
@@ -5,22 +5,22 @@ var letterChoices = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
 var wins = 0;
 var chances = 15;
 var guesses = [];
-
-//Game start display
 var currentWord = availableWords[Math.floor(Math.random() * availableWords.length)];
 var blanks = currentWord.split("").map(function(){return "_"})
+
+//Game start display
 $(".current-word").html("<h4>" + "Current Beer Style<br/>" + blanks.join(" ") + "</h4>");
 
 //User guesses to correct word
 document.onkeyup = function(event) {
 	var finder = currentWord.indexOf(userChoice);
-	var userChoice = "";
 	var userChoice = event.key;
+	console.log(currentWord);
 	
 	while (finder > -1) {
 		blanks[finder] = userChoice;
-		var finder = currentWord.indexOf(userChoice,finder + 1);
-		$(".current-word").append("<h4>" + userChoice + "</h4>");
+		finder = currentWord.indexOf(userChoice,finder + 1);
+		$(".current-word").html("<h4>" + userChoice + "</h4>");
 	}
 
 //Letters Already Guessed
@@ -34,13 +34,15 @@ document.onkeyup = function(event) {
 	$(".guesses-remaining").html(chances);
 	if (chances === 0) {
 		alert("Booooooo, you lose. Go try different beer styles then return for more.");
+		
 		//Reset game
 		function reset() {
 		var chances = 15;	
+		var guesses = 0;
+		
 		//Pick a random word from words array
 		currentWord = availableWords[Math.floor(Math.random()* words.length)];
 		for (var i = 0; i < currentWord.length; i++) {
-		//Replace all letters in word with "_"
 		blanks[i] = "_";
 		}
 	}
